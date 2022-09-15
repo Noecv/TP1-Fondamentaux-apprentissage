4be633e895d48ef67cfa92ace2218031dcf69449
Nathan <amsellem.n@gmail.com>
2022-09-16 01:32:18 +0200

    reshape dates/heures
    Meilleure lisibilité du code pour la mise en forme des données : on delete la première ligne direct

--- testopendata.py
+++ testopendata.py
@@ -4,24 +4,21 @@ from datetime import *
 reader = csv.reader(open("Occupancy_Estimation.csv", "r"),
                     delimiter=",")
 x = list(reader)
-
+# delete la premiere ligne
+# delete la derniere colonne : fait office de sortie
 x = numpy.delete(x, -1, axis=1)
-print(numpy.shape(x))
-# ligne servant à limiter la taille des données (donc des calculs) lors des tests
-#x = x[0:2000]
-print(numpy.shape(x))
+x = numpy.delete(x, 0, 0)
+
 # On extraie la colonne des dates
 dates = [i[0] for i in x]
 
 # Permet de transformer un string en type date
-
-
 def str_date_to_datatime(str_date):
     return datetime.strptime(str_date, '%Y/%m/%d')
 
 
 # on transforme notre tableau de dates de type string en dates de type date
-for i in range(1, int(len(dates))):
+for i in range(0, int(len(dates))):
     dates[i] = str_date_to_datatime(dates[i])
 
 # Permet de tranformer une date en int
@@ -32,7 +29,7 @@ def date_to_integer(date):
 
 
 # on transforme notre tableau de dates de type date en dates de type int
-for i in range(1, int(len(dates))):
+for i in range(0, int(len(dates))):
     dates[i] = date_to_integer(dates[i])
 
 # Permet de transformer nos dates en nombre de jours effectifs sur lesquels les mesures sont effectuées
@@ -40,7 +37,7 @@ for i in range(1, int(len(dates))):
 
 def date_to_days(tab_date):
     day = numpy.zeros(int(len(tab_date)))
-    for i in range(2, int(len(tab_date))):
+    for i in range(1, int(len(tab_date))):
         if tab_date[i] > tab_date[i-1]:
             day[i] += day[i-1]+1
         else:
@@ -63,14 +60,14 @@ def timeToFloat(time):
 times = [i[1] for i in x]
 
 # Rempli les heures normalisées de 0 à 1
-for i in range(1, int(len(times))):
+for i in range(0, int(len(times))):
     times[i] = timeToFloat(times[i])
 times = numpy.array(times)
 
 # transforme notre tableau en numpy array
 x = numpy.array(x)
-# delete la premiere ligne, et les deux premieres colonnes car nous venons de transformer les types des dates et des horaires
-x = numpy.delete(x, 0, 0)
+
+# #delete les deux premieres colonnes car nous venons de transformer les types des dates et des horaires
 x = numpy.delete(x, 0, 1)
 x = numpy.delete(x, 0, 1)
 
@@ -78,12 +75,9 @@ x = numpy.delete(x, 0, 1)
 dates_times = numpy.zeros((int(len(dates)), 2))
 
 # On remplis le tableau avec les dates et les heures
-for i in range(1, int(len(dates))):
+for i in range(0, int(len(dates))):
     dates_times[i][0] = dates[i]
     dates_times[i][1] = times[i]
-
-# on supprime la première ligne qui est vide
-dates_times = numpy.delete(dates_times, 0, 0)
 
 # on concatene les deux tableaux
 x = numpy.concatenate((dates_times, x), axis=1)
@@ -104,11 +98,7 @@ covx = numpy.cov(x, rowvar=False)
 # eigen value of covx
 valeurs_propres, vecteurs_propres = numpy.linalg.eig(covx)
 
-#numpy.extract(vecteurs_propres < 1, vecteurs_propres)
-
 # calculate featureVector of eigenvalue
-
-
 FeatureVector = numpy.zeros((0, p))
 
 for i in range(0, len(valeurs_propres)):
